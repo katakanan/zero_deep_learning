c6c069d397f54df50a6709fd1abcefb06760dd96
katakanan <>
2025-01-21 10:08:45 +0900

    test predict normalized arg is important

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import sys, os
 sys.path.append(os.pardir)
 import numpy as np
 from dataset.mnist import load_mnist
-
+import pickle
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
+    # return np.exp(np.minimum(x, 0)) / (1 + np.exp(- np.abs(x)))
 
 def identity_function(x):
     return x
@@ -18,17 +19,16 @@ def softmax(a):
     return y
 
 def init_network():
-    network = {}
-    network['W1'] = np.array([[0.1, 0.3, 0.5],[0.2, 0.4, 0.6]])
-    network['b1'] = np.array([0.1, 0.2, 0.3])
-    network['W2'] = np.array([[0.1, 0.4],[0.2, 0.5],[0.3, 0.6]])
-    network['b2'] = np.array([0.1, 0.2])
-    network['W3'] = np.array([[0.1, 0.3],[0.2, 0.4]])
-    network['b3'] = np.array([0.1, 0.2])
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
 
     return network
 
-def forward(network, x):
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
+
+def predict(network, x):
     W1, W2, W3 = network['W1'], network['W2'], network['W3']
     b1, b2, b3 = network['b1'], network['b2'], network['b3']
     a1 = np.dot(x, W1) + b1
@@ -41,8 +41,14 @@ def forward(network, x):
     return y
 
 if __name__ == "__main__":
-    (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
-    print(x_train.shape)
-    print(t_train.shape)
-    print(x_test.shape)
-    print(t_test.shape)
+    x, t = get_data()
+    network = init_network()
+
+    accuracy_cnt = 0
+    for i in range(len(x)):
+        y = predict(network, x[i])
+        p = np.argmax(y)
+        if p == t[i]:
+            accuracy_cnt += 1
+
+    print("Accuracy:" + str(float(accuracy_cnt)/len(x)))
