d455f2cafa35a8b532cefc68b70398c63de5a75e
katakanan <>
2025-01-22 09:48:26 +0900

    cross entropy batch

--- ch04/main.py
+++ ch04/main.py
@@ -4,18 +4,15 @@ sys.path.append(os.getcwd())
 from dataset.mnist import load_mnist
 import numpy as np
 import pickle
-
-def sum_square_error(y, t):
-    return 0.5*np.sum((y-t)**2)
-
-def cross_entropy_error(y, t):
-    delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
+from common.functions import softmax, cross_entropy_error
 
 if __name__ == "__main__":
-    y = np.array([0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0])
-    t = np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0]) #2 is correct answer
-    print(cross_entropy_error(y, t))
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+    print(x_train.shape)
+    print(t_train.shape)
 
-    y = np.array([0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0])
-    print(cross_entropy_error(y, t))
+    train_size = x_train.shape[0]
+    batch_size = 10
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
