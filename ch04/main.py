8dfb5ec52a9b761f2d9ded971342e727de19a470
katakanan <>
2025-01-23 09:33:13 +0900

    Twolayernet

--- ch04/main.py
+++ ch04/main.py
@@ -4,7 +4,7 @@ sys.path.append(os.getcwd())
 from dataset.mnist import load_mnist
 import numpy as np
 import pickle
-from common.functions import softmax, cross_entropy_error
+from common.functions import softmax, cross_entropy_error, sigmoid
 from common.gradient import numerical_diff, numerical_gradient
 
 def function_1(x):
@@ -36,15 +36,48 @@ class SimpleNet:
         loss = cross_entropy_error(y, t)
         return loss
     
-if __name__ == "__main__":
-    net = SimpleNet()
-    print(net.W)
-    x = np.array([0.6, 0.9])
-    p = net.predict(x)
-    print(p)
-    print(np.argmax(p))
-    t = np.array([0, 0, 1])
-    print(net.loss(x, t))
 
-    f = lambda w: net.loss(x, t)
-    print(numerical_gradient(f, net.W))
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+    def predict(self, x):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+                                                
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = sigmoid(a2)
+        return y
+    
+    def loss(self, x, t):
+        y = self.predict(x)
+        return cross_entropy_error(y, t)
+    
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.array(y, axis=1)
+        t = np.argmax(t, axis=1)
+        accuracy = np.sum(y == t)/float(x.shape[0])
+        return accuracy
+    
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+        return grads
+
+if __name__ == "__main__":
+    net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
+    print(net.params['W1'].shape)
+    print(net.params['b1'].shape)
+    print(net.params['W2'].shape)
+    print(net.params['b2'].shape)
