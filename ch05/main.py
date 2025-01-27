c2c8381aa57cf25af18067fb3c65bfdda6df838d
katakanan <>
2025-01-27 15:31:36 +0900

    add lyer sample

--- ch05/main.py
+++ ch05/main.py
@@ -27,18 +27,44 @@ class AddLayer:
         dy = dout
         return dx, dy
     
+class Relu:
+    def __init__(self):
+        self.max = None
+    
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+    
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+
+
+
 if __name__ == "__main__":
     apple = 100
     apple_num = 2
+    orange = 150
+    orange_num = 3
     tax = 1.1
     mul_apple_layer = MulLayer()
+    mul_orange_layer = MulLayer()
+    add_apple_orange_layer = AddLayer()
     mul_tax_layer = MulLayer()
 
     apple_price = mul_apple_layer.forward(apple, apple_num)
-    price = mul_tax_layer.forward(apple_price, tax)
-    print(price)
+    orange_price = mul_orange_layer.forward(orange, orange_num)
+    all_price = add_apple_orange_layer.forward(apple_price, orange_price)
+    price = mul_tax_layer.forward(all_price, tax)
 
     dprice = 1
-    dapple_price, dtax = mul_tax_layer.backward(dprice)
+    dall_price, dtax = mul_tax_layer.backward(dprice)
+    dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price)
+    dorange, dorange_num = mul_orange_layer.backward(dorange_price)
     dapple, dapple_num = mul_apple_layer.backward(dapple_price)
-    print(dapple, dapple_num, dtax)
+
+    print(price)
+    print(dapple_num, dapple, dorange, dorange_num, dtax)
