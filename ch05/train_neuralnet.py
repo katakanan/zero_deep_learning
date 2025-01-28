9e0a32f869ffdea2cba8251abbd82695073d98ba
katakanan <>
2025-01-28 10:31:27 +0900

    train net

--- ch05/train_neuralnet.py
+++ ch05/train_neuralnet.py
@@ -6,10 +6,12 @@ from dataset.mnist import load_mnist
 from two_layer_net import TwoLayerNet
 
 if __name__ == "__main__":
+    # データの読み込み
     (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
     network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
-    iters_num = 10000
+    iters_num = 5000
     train_size = x_train.shape[0]
     batch_size = 100
     learning_rate = 0.1
@@ -24,18 +26,19 @@ if __name__ == "__main__":
         batch_mask = np.random.choice(train_size, batch_size)
         x_batch = x_train[batch_mask]
         t_batch = t_train[batch_mask]
-
+        
         grad = network.gradient(x_batch, t_batch)
 
         for key in ('W1', 'b1', 'W2', 'b2'):
-            network.params[key] -= learning_rate*grad[key]
-
+            network.params[key] -= learning_rate * grad[key]
+        
         loss = network.loss(x_batch, t_batch)
+        # print(loss)
         train_loss_list.append(loss)
-
+        
         if i % iter_per_epoch == 0:
             train_acc = network.accuracy(x_train, t_train)
             test_acc = network.accuracy(x_test, t_test)
             train_acc_list.append(train_acc)
             test_acc_list.append(test_acc)
-            print(train_acc, test_acc)
+            print("hello", train_acc, test_acc)
